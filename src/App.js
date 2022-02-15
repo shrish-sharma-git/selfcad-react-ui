59200b992f7fb318eda39d5ab85cdc5f3c3a7a4f
Shrish Sharma <shrish0608@gmail.com>
2022-02-15 10:38:06 +0530

    Add Project Button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import ProjectCard from "./components/ProjectCard";
 import Jumbotron from "./components/Jumbotron";
 import './app.css'
+import AddProjectCard from "./components/AddProjectCard";
 
 function App() {
   return (
     <div>
       <Jumbotron />
       <div className="project-section">
+        <AddProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
         <ProjectCard />
         
       </div>
---
+++ src/components/AddProjectCard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AddProjectCard = () => {
+    return (  
+        <div className="box folded-box">
+            <span className='content'>Add Project</span>
+            <button className='btn-add'>+</button>
+        </div>
+    );
+}
+ 
+export default AddProjectCard;
